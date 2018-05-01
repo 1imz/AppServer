19bf4340fbf7b4838a1e3a11ba51f793794cc485
1imz <imran_bhuta@hotmail.co.uk>
2018-05-01 11:01:28 -0400

    Ajax server test

--- testAJAX.js
+++ testAJAX.js
@@ -1,8 +1,7 @@
 var xhr; // define the global variable to process the AJAX request
 function callDivChange() {
 xhr = new XMLHttpRequest();
-var filename = document.getElementById("filename").value;
-xhr.open("GET", filename, true);
+xhr.open("GET", "test2.html", true);
 xhr.onreadystatechange = processDivChange;
 try {
  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
