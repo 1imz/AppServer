35ed08f59ba8d8b3de085a67252d55a2638ffc09
1imz <imran_bhuta@hotmail.co.uk>
2018-05-03 12:26:31 -0400

    PostGIS test

--- httpServer.js
+++ httpServer.js
@@ -19,7 +19,39 @@ var app = express();
 		console.log("The file " + filename + " was requested.");
 		next();
 	});
-	
+    
+// read in the file and force it to be a string by adding “” at the beginning
+	var configtext = ""+fs.readFileSync("/home/studentuser/certs/postGISConnection.js");
+
+	// now convert the configruation file into the correct format -i.e. a name/value pair array
+	var configarray = configtext.split(",");
+	var config = {};
+	for (var i = 0; i < configarray.length; i++) {
+		var split = configarray[i].split(':');
+		config[split[0].trim()] = split[1].trim();
+	}
+	var pg = require('pg');
+	var pool = new pg.Pool(config);
+	console.log(config);
+
+app.get('/postgistest', function (req,res) {
+		console.log('postgistest');
+		pool.connect(function(err,client,done) {
+		if(err){
+				   console.log("not able to get connection "+ err);
+				   res.status(400).send(err);
+			   } 
+				client.query('SELECT name FROM united_kingdom_counties' ,function(err,result) {
+				console.log("query");
+				   done(); 
+				   if(err){
+					   console.log(err);
+					   res.status(400).send(err);
+				   }
+				   res.status(200).send(result.rows);
+			   });
+			});
+	});
 
 	// add an http server to serve files to the Edge browser 
 	// due to certificate issues it rejects the https files if they are not
